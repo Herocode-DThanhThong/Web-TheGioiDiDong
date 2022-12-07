1d56f06b164bc503bfab5dd9e9914d3e2e639b05
Dương Thành Thông <duongthanhthong0611@gmail.com>
2022-12-07 20:58:18 +0700

    update upload_preset

--- client/src/services/globalServices.js
+++ client/src/services/globalServices.js
@@ -9,7 +9,7 @@ export class GlobalServices extends BaseService {
   uploadFileApi = (file) => {
     const formData = new FormData();
     formData.append("file", file);
-    formData.append("upload_preset", "upload_file_tgdd");
+    formData.append("upload_preset", "your_upload_presset");
     return axios.post("YOUR_API_IN_CLOUDINARY", formData);
   };
 
